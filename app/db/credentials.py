6237840b60886bab00db7d69a0a599f8b5de34cb
ivanpm21 <ivanpm21>
2024-10-08 18:35:32 +0200

    Data migration included in docker container

--- app/db/credentials.py
+++ app/db/credentials.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
 
 from db.db import DBCredentials
 
-load_dotenv('.env', override=True)
 
 
 def get_credentials() -> DBCredentials:
--- app/migrate/migrate_data.py
+++ app/migrate/migrate_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import psycopg as p
 
 from db.db import DatabaseConnection
 from db.credentials import get_credentials
@@ -9,10 +8,6 @@ dataframe = pd.read_csv('remote_work_productivity.csv', sep=',',thousands='.', d
 dataframe.drop('Employee_ID', axis=1, inplace=True)
 dataframe.rename(columns={'Employment_Type': 'type', 'Hours_Worked_Per_Week': 'hours_worked_per_week',
                            'Productivity_Score': 'score', 'Well_Being_Score': 'satisfaction'}, inplace=True)
-#dataframe[['agno', 'periodo']] = dataframe['Periodo'].str.split('T', n=1, expand=True)
-#dataframe.drop(['Nacionalidad', 'Periodo'], axis=1, inplace=True)
-#dataframe.rename(columns={'Sexo': 'sexo', 'Nivel de formacion alcanzado': 'formacion', 'Total': 'total'}, inplace=True)
-#dataframe[["total", "agno", "periodo"]] = dataframe[["total", "agno", "periodo"]].apply(pd.to_numeric)
 
 
 def _get_query() -> str:
